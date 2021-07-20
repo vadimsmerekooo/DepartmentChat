51cd06f361888f94837146173e00c18914c687a0
vadim.a.shkuratov@gmail.com <vadim.a.shkuratov@gmail.com>
2021-07-20 23:19:05 +0300

    Ups. don't save js and index view(

--- DepartmentChat/wwwroot/js/site.js
+++ DepartmentChat/wwwroot/js/site.js
@@ -5,27 +5,8 @@
 
 
 hubConnection.on("ReceiveMessage", function (userName, message) {
-    var blockMessage = html(
-        <div class="message message-out">
-            <a href="#" data-bs-toggle="modal" data-bs-target="#modal-profile" class="avatar avatar-responsive">
-                <img class="avatar-img" src="assets/img/avatars/1.jpg" alt="">
-                            </a>
-
-                <div class="message-inner">
-                    <div class="message-body">
-                        <div class="message-content">
-                            <div class="message-text">
-                                <p>Hey, Marshall! How are you? Can you please change the color theme of the website to pink and purple? 😂</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="message-footer">
-                        <span class="extra-small text-muted">08:45 PM</span>
-                    </div>
-                </div>
-                        </div>    );
-    document.getElementById("chatroom").insertAdjacentHTML('beforeend', "<p>" + userName + ": " + message + "</p>");
+    var blockMessage = html('');
+    document.getElementById("chatroom").insertAdjacentHTML('beforeend', blockMessage);
 
 });
 
