c710144730fba8445d618924b5412a6590be07a6
vadim.a.shkuratov@gmail.com <vadim.a.shkuratov@gmail.com>
2021-07-20 17:11:47 +0300

    atempt add chats group

--- DepartmentChat/Services/ChatHub.cs
+++ DepartmentChat/Services/ChatHub.cs
@@ -6,9 +6,23 @@ namespace DepartmentChat.Services
 {
     public class ChatHub : Hub
     {
-        public void Send( string message)
+        //public void Send( string message)
+        //{
+        //    Clients.All.SendAsync("Send", message);
+        //}
+        public ChatHub()
         {
-            Clients.All.SendAsync("Send", message);
+            Join();
+        }
+
+        public Task Join()
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, "foo");
+        }
+
+        public async Task Send(string message)
+        {
+            await Clients.Group("foo").SendAsync("Send", Context.User.Identity.Name + " joined.");
         }
     }
 }
